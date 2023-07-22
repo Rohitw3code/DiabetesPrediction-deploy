9d5585e049d5a7d64de19dfc8b7230c4de21c39c
Beluga <rohitcode005@gmail.com>
2023-07-23 02:03:19 +0530

    pred page

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import pandas as pd
+import pickle
 
 # Initializing Flask
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+# Load the pickle model
+with open("model_file.pkl", "rb") as file:
+    loaded_model = pickle.load(file)
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+features = {}
+
+@app.route('/process_text', methods=['POST'])
+def process_text():
+    if request.method == 'POST':
+        features['encoded_OverTime'] = request.form['encoded_OverTime']
+        features['Age'] = request.form['age']
+        features['JobInvolvement'] = request.form['job_involvement']
+        features['JobLevel'] = request.form['job_level']
+        features['MonthlyIncome'] = request.form['monthly_income']
+        features['TotalWorkingYears'] = request.form['total_working_years']
+        features['YearsAtCompany'] = request.form['years_at_company']
+        features['YearsInCurrentRole'] = request.form['years_in_currentRole']
+        features['encoded_MaritalStatus'] = request.form['marital_status']
+        features['YearsWithCurrManager'] = request.form['years_with_curr_manager']
+        p_df = pd.DataFrame(features,index=[0])
+        return f"Text input received: {'Yes' if int(loaded_model.predict(p_df)[0]) == 1 else 'No' }"
+    else:
+        return "Method not allowed"
 
 
 if __name__ == "__main__":
