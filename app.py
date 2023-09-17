f4bde07f09488f62b691fde0dbf43ba0c0a3e450
rohitw3code <rohitcode005@gmail.com>
2023-09-18 00:52:43 +0530

    updated

--- app.py
+++ app.py
@@ -6,22 +6,8 @@ import pickle
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 # Load the pickle model
-with open("model_file.pkl", "rb") as file:
+with open("model.pkl", "rb") as file:
     loaded_model = pickle.load(file)
-
-
-file_path = 'label_encoder.pkl'
-# Load the LabelEncoder from the saved file
-with open(file_path, 'rb') as file:
-    loaded_label_encoder = pickle.load(file)
-
-
-file_path = 'min_max_scaler.pkl'
-# Load the MinMaxScaler from the saved file
-with open(file_path, 'rb') as file:
-    loaded_scaler = pickle.load(file)
-
-
 
 @app.route("/")
 def home():
@@ -32,31 +18,25 @@ features = {}
 @app.route('/process_text', methods=['POST'])
 def process_text():
     if request.method == 'POST':
-        features['encoded_OverTime'] = request.form['encoded_OverTime']
-        features['Age'] = request.form['age']
-        features['JobInvolvement'] = request.form['job_involvement']
-        features['JobLevel'] = request.form['job_level']
-        features['MonthlyIncome'] = request.form['monthly_income']
-        features['TotalWorkingYears'] = request.form['total_working_years']
-        features['YearsAtCompany'] = request.form['years_at_company']
-        features['YearsInCurrentRole'] = request.form['years_in_currentRole']
-        features['encoded_MaritalStatus'] = request.form['marital_status']
-        features['YearsWithCurrManager'] = request.form['years_with_curr_manager']
-        p_df = pd.DataFrame(features,index=[0])
+        features['Pregnancies'] = request.form['Pregnancies']
+        features['PlasmaGlucose'] = request.form['PlasmaGlucose']
+        features['DiastolicBloodPressure'] = request.form['DiastolicBloodPressure']
+        features['TricepsThickness'] = request.form['TricepsThickness']
+        features['Serumlnsulin'] = request.form['Serumlnsulin']
+        features['BMI'] = request.form['BMI']
+        features['DiabetesPedigree'] = request.form['DiabetesPedigree']
+        features['Age'] = request.form['Age']
+        
+        data_dict = {key: value for key, value in features.items()}
+        
+        p_df = pd.DataFrame(data_dict,index=[0],columns=['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure','TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age'])
 
-        # LabelEncoder
-        columns = ['encoded_MaritalStatus','encoded_OverTime']
-        
-        for col in columns:
-            p_df[col] = loaded_label_encoder.fit_transform(p_df[col])
-        
-        scaled_data = loaded_scaler.fit_transform(p_df)
-        new_df = pd.DataFrame(scaled_data, columns=p_df.columns)
-        pred_v = 'Yes' if int(loaded_model.predict(new_df)[0]) == 1 else 'No'
+        p_df = p_df.apply(pd.to_numeric, errors='coerce', downcast='integer')        
+        pred_v = 'Yes' if int(loaded_model.predict(p_df)[0]) == 1 else 'No'
         return render_template("output.html",pred=pred_v)
     else:
         return "Method not allowed"
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5001)
+    app.run(port=5001)
